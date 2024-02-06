49d813a7d625be116ba85fa32afaf3f917d5179a
inha-cvl <cvlab.inhauniv@gmail.com>
2024-02-06 17:49:39 +0900

    ros_ws

---
+++ ros_ws/build/agx_cam2ros/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/inha/Documents/vision/ros_ws/src/agx_cam2ros/include".split(';') if "/home/inha/Documents/vision/ros_ws/src/agx_cam2ros/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "agx_cam2ros"
+PROJECT_SPACE_DIR = "/home/inha/Documents/vision/ros_ws/devel"
+PROJECT_VERSION = "0.0.0"
---
+++ ros_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/inha/Documents/vision/ros_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/inha/tod_ioniq5_ws/devel;/home/inha/catkin_ws/devel;/home/inha/Documents/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/inha/tod_ioniq5_ws/devel;/home/inha/catkin_ws/devel;/home/inha/Documents/catkin_ws/devel;/opt/ros/noetic' != '' else []
